31e6e6a23e2d969f8d78aeec01d8faf2f3195ed5
Li Wei <everwanna@gmail.com>
2016-05-05 16:55:06 +0800

    fixed vendor key too long in nodejs test case

--- nodejs/test/DynamicKeyTest.js
+++ nodejs/test/DynamicKeyTest.js
@@ -2,8 +2,8 @@
 var DynamicKey = require('../src/DynamicKey');
 var DynamicKey3 = require('../src/DynamicKey3');
 var DynamicKey4 = require('../src/DynamicKey4');
-var vendor = "970ca35de60c44645bbae84215061b33b";
-var key = "5cfd2fd1755d40ecb72977518be15d3b";
+var vendor  = "970ca35de60c44645bbae8a215061b33";
+var key     = "5cfd2fd1755d40ecb72977518be15d3b";
 var channel = "7d72365eb983485397e3e3f9d460bdda";
 var ts = 1446455472;
 var r = 58964981;
@@ -12,7 +12,7 @@ var uid=2882341273;
 var expiredTs=1446455471;
 
 exports.DynamicKeyTest = function (test) {
-  var expected = "70e0e5b0a137630190bfa475506de9fb56012576970ca35de60c44645bbae84215061b33b14464554720383bbf5";
+  var expected = "870588aad271ff47094eb622617e89d6b5b5a615970ca35de60c44645bbae8a215061b3314464554720383bbf5";
   var actual = DynamicKey.generate(vendor, key, channel, ts, r);
   test.equal(expected, actual);
   test.done();
@@ -20,7 +20,7 @@ exports.DynamicKeyTest = function (test) {
 
 
 exports.DynamicKey3Test = function (test) {
-  var expected = "00381e1588b09e6e76a0d2e8fd9e5257dbddd9c60a0970ca35de60c44645bbae84215061b33b14464554720383bbf528823412731446455471";
+  var expected = "0037666966591a93ee5a3f712e22633f31f0cbc8f13970ca35de60c44645bbae8a215061b3314464554720383bbf528823412731446455471";
   var actual = DynamicKey3.generate(vendor, key, channel, ts, r, uid, expiredTs);
   test.equal(expected, actual);
   test.done();
@@ -28,14 +28,14 @@ exports.DynamicKey3Test = function (test) {
 
 
 exports.RecordingKeyTest = function (test) {
-  var expected = "0040f6609180e7967c4047f88fcc8c187e77a4d22a4970ca35de60c44645bbae84215061b33b14464554720383bbf51446455471";
+  var expected = "004e0c24ac56aae05229a6d9389860a1a0e25e56da8970ca35de60c44645bbae8a215061b3314464554720383bbf51446455471";
   var actual = DynamicKey4.generateRecordingKey(vendor, key, channel, ts, r, uid, expiredTs);
   test.equal(expected, actual);
   test.done();
 };
 
 exports.MediaChannelKeyTest = function (test) {
-  var expected = "004b3e0a78e01b5a07a4627b03ff1bdcc4330839c9a970ca35de60c44645bbae84215061b33b14464554720383bbf51446455471";
+  var expected = "004d0ec5ee3179c964fe7c0485c045541de6bff332b970ca35de60c44645bbae8a215061b3314464554720383bbf51446455471";
   var actual = DynamicKey4.generateMediaChannelKey(vendor, key, channel, ts, r, uid, expiredTs);
   test.equal(expected, actual);
   test.done();
