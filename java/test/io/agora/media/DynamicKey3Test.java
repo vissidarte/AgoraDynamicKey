3d5501fff7b45e0ec0da7a6d500368e90db7b762
Li Wei <everwanna@gmail.com>
2016-05-05 17:11:26 +0800

    fixed vendor key too long in java test case

--- java/test/io/agora/media/DynamicKey3Test.java
+++ java/test/io/agora/media/DynamicKey3Test.java
@@ -11,9 +11,9 @@ public class DynamicKey3Test {
 
     @Test
     public void testGenerate() throws Exception {
-        String vendor = "970ca35de60c44645bbae84215061b33b";
-        String key = "5cfd2fd1755d40ecb72977518be15d3b";
-        String channel = "7d72365eb983485397e3e3f9d460bdda";
+        String vendor   = "970ca35de60c44645bbae8a215061b33";
+        String key      = "5cfd2fd1755d40ecb72977518be15d3b";
+        String channel  = "7d72365eb983485397e3e3f9d460bdda";
         int ts = 1446455472;
         int r = 58964981;
         long uid = 2882341273L;
@@ -21,7 +21,7 @@ public class DynamicKey3Test {
 
         String result = DynamicKey3.generate(vendor, key, channel, ts, r, uid, expiredTs);
 
-        String expected = "00381e1588b09e6e76a0d2e8fd9e5257dbddd9c60a0970ca35de60c44645bbae84215061b33b14464554720383bbf528823412731446455471";
+        String expected = "0037666966591a93ee5a3f712e22633f31f0cbc8f13970ca35de60c44645bbae8a215061b3314464554720383bbf528823412731446455471";
         assertEquals(expected, result);
     }
 }
--- java/test/io/agora/media/DynamicKey4Test.java
+++ java/test/io/agora/media/DynamicKey4Test.java
@@ -11,30 +11,30 @@ public class DynamicKey4Test {
 
     @Test
     public void testGenerateRecordingKey() throws Exception {
-        String vendor = "970ca35de60c44645bbae84215061b33b";
-        String key = "5cfd2fd1755d40ecb72977518be15d3b";
-        String channel = "7d72365eb983485397e3e3f9d460bdda";
+        String vendor   = "970ca35de60c44645bbae8a215061b33";
+        String key      = "5cfd2fd1755d40ecb72977518be15d3b";
+        String channel  = "7d72365eb983485397e3e3f9d460bdda";
         int ts = 1446455472;
         int r = 58964981;
         long uid = 2882341273L;
         int expiredTs=1446455471;
 
-        String expected = "0040f6609180e7967c4047f88fcc8c187e77a4d22a4970ca35de60c44645bbae84215061b33b14464554720383bbf51446455471";
+        String expected = "004e0c24ac56aae05229a6d9389860a1a0e25e56da8970ca35de60c44645bbae8a215061b3314464554720383bbf51446455471";
         String result = DynamicKey4.generateRecordingKey(vendor, key, channel, ts, r, uid, expiredTs);
         assertEquals(expected, result);
     }
 
     @Test
     public void testGenerateMediaChannelKey() throws Exception {
-        String vendor = "970ca35de60c44645bbae84215061b33b";
-        String key = "5cfd2fd1755d40ecb72977518be15d3b";
-        String channel = "7d72365eb983485397e3e3f9d460bdda";
+        String vendor   = "970ca35de60c44645bbae8a215061b33";
+        String key      = "5cfd2fd1755d40ecb72977518be15d3b";
+        String channel  = "7d72365eb983485397e3e3f9d460bdda";
         int ts = 1446455472;
         int r = 58964981;
         long uid = 2882341273L;
         int expiredTs=1446455471;
 
-        String expected = "004b3e0a78e01b5a07a4627b03ff1bdcc4330839c9a970ca35de60c44645bbae84215061b33b14464554720383bbf51446455471";
+        String expected = "004d0ec5ee3179c964fe7c0485c045541de6bff332b970ca35de60c44645bbae8a215061b3314464554720383bbf51446455471";
         String result = DynamicKey4.generateMediaChannelKey(vendor, key, channel, ts, r, uid, expiredTs);
         assertEquals(expected, result);
     }
