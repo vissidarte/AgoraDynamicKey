0ee8fc246ea06db9f83f574f3d2c5950878c65d5
Li Wei <everwanna@gmail.com>
2016-05-14 04:36:39 +0000

    add test case for negative random int

--- python/test/DynamicKey4Test.py
+++ python/test/DynamicKey4Test.py
@@ -1,6 +1,8 @@
 import sys
 import unittest
 import os
+import time
+from random import randint
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 from DynamicKey4 import generateRecordingKey
@@ -9,20 +11,20 @@ from DynamicKey4 import generateMediaChannelKey
 statickey   = "970ca35de60c44645bbae8a215061b33"
 signkey     = "5cfd2fd1755d40ecb72977518be15d3b"
 channelname = "7d72365eb983485397e3e3f9d460bdda"
-unixts = 1446455472
+unixts = 1463199341
 uid = 2882341273
-randomint = 58964981
-expiredts = 1446455471
+randomint = -252347445
+expiredts = 0
 
 class DynamicKeyTest(unittest.TestCase):
     def test_recording(self):
-        expected = "004e0c24ac56aae05229a6d9389860a1a0e25e56da8970ca35de60c44645bbae8a215061b3314464554720383bbf51446455471";
+        expected = "004f0386ad262b31103ec737f06c303f778c2410fed970ca35de60c44645bbae8a215061b331463199341f0f57bcb0000000000";
         actual = generateRecordingKey(statickey, signkey, channelname, unixts, randomint, uid, expiredts);
         print actual
         self.assertEqual(expected, actual)
 
     def test_mediachannel(self):
-        expected = "004d0ec5ee3179c964fe7c0485c045541de6bff332b970ca35de60c44645bbae8a215061b3314464554720383bbf51446455471";
+        expected = "0043d8758ab4f38a56400c7bbfaae66d7ff4b5bea26970ca35de60c44645bbae8a215061b331463199341f0f57bcb0000000000";
         actual = generateMediaChannelKey(statickey, signkey, channelname, unixts, randomint, uid, expiredts);
         print actual
         self.assertEqual(expected, actual)
