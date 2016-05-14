dced3307ce7093b3e0445f1b8c0677df70800d76
Li Wei <everwanna@gmail.com>
2016-05-14 06:27:39 +0000

    simplify sample code

--- cpp/sample/main.cpp
+++ cpp/sample/main.cpp
@@ -7,33 +7,18 @@
 #include <iostream>
 #include <cstdint>
 
-void generateRecordingKey(){
-    auto staticKey  = "970ca35de60c44645bbae8a215061b33";
-    auto  signKey   = "5cfd2fd1755d40ecb72977518be15d3b";
-    auto channelName= "my channel name for recording";
-    auto  unixTs = ::time(NULL);
-    int randomInt = (::rand()%256 << 24) + (::rand()%256 << 16) + (::rand()%256 << 8) + (::rand()%256);
-    uint32_t uid = 2882341273u;
-    auto  expiredTs = 0;
-    std::string result = agora::tools::DynamicKey4::generateRecordingKey(staticKey, signKey, channelName, unixTs, randomInt, uid, expiredTs);
-    std::cout << result << std::endl;
-}
-
-void generateMediaKey(){
-    auto staticKey  = "970ca35de60c44645bbae8a215061b33";
-    auto  signKey   = "5cfd2fd1755d40ecb72977518be15d3b";
-    auto channelName= "my channel name for media service";
-    auto  unixTs = ::time(NULL);
-    int randomInt = (::rand()%256 << 24) + (::rand()%256 << 16) + (::rand()%256 << 8) + (::rand()%256);
-    uint32_t uid = 2882341273u;
-    auto  expiredTs = 0;
-    std::string result = agora::tools::DynamicKey4::generateMediaChannelKey(staticKey, signKey, channelName, unixTs, randomInt, uid, expiredTs);
-    std::cout << result << std::endl;
-}
-
 int main(int argc, char const *argv[]) {
   ::srand(::time(NULL));
-  generateRecordingKey();
-  generateMediaKey();
+
+  auto staticKey  = "970ca35de60c44645bbae8a215061b33";
+  auto  signKey   = "5cfd2fd1755d40ecb72977518be15d3b";
+  auto channelName= "my channel name for recording";
+  auto  unixTs = ::time(NULL);
+  int randomInt = (::rand()%256 << 24) + (::rand()%256 << 16) + (::rand()%256 << 8) + (::rand()%256);
+  uint32_t uid = 2882341273u;
+  auto  expiredTs = 0;
+
+  std::cout << agora::tools::DynamicKey4::generateRecordingKey(staticKey, signKey, channelName, unixTs, randomInt, uid, expiredTs) << std::endl;
+  std::cout << agora::tools::DynamicKey4::generateMediaChannelKey(staticKey, signKey, channelName, unixTs, randomInt, uid, expiredTs) << std::endl;
   return 0;
 }
