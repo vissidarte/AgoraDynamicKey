c24485f0e5ae0ee658d64e41a0ca0018265db229
linshibo <linshibo@gmail.com>
2017-01-22 14:05:17 +0800

    feature(python):sample.py support for parameter

--- python/sample/sample.py
+++ python/sample/sample.py
@@ -1,3 +1,7 @@
+#! /usr/bin/python
+#! -*- coding: utf-8 -*-
+
+import argparse
 import sys
 import os
 import time
@@ -7,16 +11,36 @@ sys.path.append(os.path.join(os.path.dirname(__file__), '../src'))
 from DynamicKey4 import generateRecordingKey
 from DynamicKey4 import generateMediaChannelKey
 
-appID   = "970ca35de60c44645bbae8a215061b33"
-appCertificate     = "5cfd2fd1755d40ecb72977518be15d3b"
-channelname = "7d72365eb983485397e3e3f9d460bdda"
-unixts = int(time.time());
-uid = 2882341273
-randomint = -2147483647
-expiredts = 0
+def main():
+  unixts = int(time.time())
+  randomint = -2147483647
 
-print "%.8x" % (randomint & 0xFFFFFFFF)
+  parser = argparse.ArgumentParser(description='Generator for channel key')
+  parser.add_argument("--app_id", dest="app_id", default="")
+  parser.add_argument("--certificate", dest="certificate", default="")
+  parser.add_argument("--channel_name", dest="channel_name", default="")
+  parser.add_argument("--uid", dest="uid", type=int, default=0)
+  parser.add_argument("--expired_ts", dest="expired_ts", type=int, default=0)
+
+  args = parser.parse_args()
+
+  if not args.app_id:
+    print >>sys.stderr, "No APP ID is designated"
+    return -1
+
+  if not args.certificate:
+    print >>sys.stderr, "No certificate is designated"
+    return -1
+
+  if not args.channel_name:
+    print >>sys.stderr, "No channel name is degsignated"
+    return -1
+
+  print "Recording key:", generateRecordingKey(args.app_id, args.certificate, \
+      args.channel_name, unixts, randomint, args.uid, args.expired_ts)
+
+  print "Channel key:", generateMediaChannelKey(args.app_id, args.certificate, \
+      args.channel_name, unixts, randomint, args.uid, args.expired_ts)
 
 if __name__ == "__main__":
-    print generateRecordingKey(appID, appCertificate, channelname, unixts, randomint, uid, expiredts)
-    print generateMediaChannelKey(appID, appCertificate, channelname, unixts, randomint, uid, expiredts)
+  main()
