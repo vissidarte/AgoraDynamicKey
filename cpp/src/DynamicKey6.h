352efe9d5dba936f9a9552d923a6aca7e25693c1
linshibo <linshibo@gmail.com>
2017-12-04 09:15:40 +0000

    support dynamickey6

--- cpp/src/DynamicKey6.h
+++ cpp/src/DynamicKey6.h
@@ -1,11 +1,16 @@
 #pragma once
 
+#include <zlib.h>
+#include <cstdlib>
 #include <iostream>
-#include "Packer.h"
-#include "utils.h"
+#include <map>
+#include <string>
+#include <utility>
+#include "AgoraDynamicKey/cpp/src/utils.h"
 
 namespace agora {
 namespace tools {
+
 struct DynamicKey6 {
   enum Privileges {
     kJoinChannel = 1,
@@ -25,14 +30,10 @@ struct DynamicKey6 {
     kAdministrateChannel = 101,
   };
 
-  typedef std::map<uint16_t, std::string> PrevilidgeMessageMap;
+  typedef std::map<uint16_t, uint32_t> PrevilidgeMessageMap;
 
-  TOOLS_DECLARE_PACKABLE_3(PrevilidgeMessage, uint32_t, salt, uint32_t ts,
+  TOOLS_DECLARE_PACKABLE_3(PrevilidgeMessage, uint32_t, salt, uint32_t, ts,
                            PrevilidgeMessageMap, messages);
-
-  TOOLS_DECLARE_PACKABLE_5(DynamicKey6Content, std::string, appID, std::string,
-                           signature, std::string, message, uint32_t,
-                           crcChannelName, uint32_t, crcUid)
 
   static std::string Version() { return "006"; }
 
@@ -41,12 +42,13 @@ struct DynamicKey6 {
                                        const std::string& channelName,
                                        const std::string& uid,
                                        const std::string& message) {
+    
     std::stringstream ss;
-    ss << appId << message << channelName
-       << uid return stringToHEX(
-              hmac_sign2(appCertificate, ss.str(), HMAC_LENGTH));
+    ss << appID << channelName << uid << message;
+    return stringToHEX(hmac_sign2(appCertificate, ss.str(), HMAC_LENGTH));
   }
 
+  DynamicKey6() : crc_channel_name_(0), crc_uid_(0) {}
 
   DynamicKey6(const std::string& appId, const std::string& appCertificate,
               const std::string& channelName, uint32_t uid = 0)
@@ -54,60 +56,65 @@ struct DynamicKey6 {
         app_cert_(appCertificate),
         channel_name_(channelName),
         crc_channel_name_(0),
-        crc_uid_(0)
-  {
-
+        crc_uid_(0) {
     std::stringstream uidStr;
-    uidStr << uid ;
+    uidStr << uid;
     uid_ = uidStr.str();
-    srand(time(NULL));
+    uint32_t now = time(NULL);
+    std::srand(now);
     message_.salt = rand();
+    message_.ts = now + 34 * 3600;
   }
+
   DynamicKey6(const std::string& appId, const std::string& appCertificate,
-              const std::string& channelaName, const std::string& user = "")
+              const std::string& channelName, const std::string& uid = "")
       : app_id_(appId),
         app_cert_(appCertificate),
         channel_name_(channelName),
         uid_(uid),
         crc_channel_name_(0),
-        crc_uid_(0)
-        {
+        crc_uid_(0) {
     uint32_t now = time(NULL);
-    srand(now);
+    std::srand(now);
     message_.salt = rand();
     message_.ts = now + 34 * 3600;
   }
 
   std::string Build() {
-    if (!isUUID(app_id_)) {
+    if (!IsUUID(app_id_)) {
       perror("invalid appID");
       return "";
     }
 
-    if (!isUUID(app_cert_)) {
+    if (!IsUUID(app_cert_)) {
       perror("invalid appCertificate");
       return "";
     }
-    try{
-      std::stoi(uid_); 
-    } catch{
+    try {
+      std::stoul(uid_);
+    } catch (...) {
       perror("invalid uid");
-      return false;
+      return "";
     }
     message_raw_content_ = Pack(message_);
-    signature_ = generateSignature(app_cent_, app_id_, channel_name_, uid_, message_raw_content_ );
-    crc_channel_name_ =
-        crc32(0, channel_name_.c_str(), channel_name_.length());
-    crc_uid_ = crc32(0, uid_.c_str(), uid_.length());
-    DynamicKey6Content result(app_id_, signature_, base64Encode(message_raw_content_), crc_channel_name_, crc_uid_);
-    return Pack(result)
+    signature_ = GenerateSignature(app_cert_, app_id_, channel_name_, uid_,
+                                   message_raw_content_);
+    crc_channel_name_ = crc32(
+        0, reinterpret_cast<Bytef*>(const_cast<char*>(channel_name_.c_str())),
+        channel_name_.length());
+    crc_uid_ =
+        crc32(0, reinterpret_cast<Bytef*>(const_cast<char*>(uid_.c_str())),
+              uid_.length());
+
+    std::stringstream ss;
+    ss << DynamicKey6::Version() << signature_ << app_id_ << std::setfill('0')
+       << std::setw(10) << crc_channel_name_ << std::setfill('0')
+       << std::setw(10) << crc_uid_ << base64Encode(message_raw_content_);
+    return ss.str();
   }
 
   int AddPrivilege(Privileges privilege, uint32_t timeoutFromNow = 0) {
-    uint32_t now = time(NULL);
-    srand(now);
-    message_.salt = rand();
-    message_.messages.insert(std::make_par(privilege, timeoutFromNow));
+    message_.messages.insert(std::make_pair(privilege, timeoutFromNow));
   }
 
   int SetDynamicKeyTimeout(uint32_t timeoutFromNow) {
@@ -115,28 +122,29 @@ struct DynamicKey6 {
   }
 
   bool FromString(const std::string& channelKeyString) {
-    if (channelKeyString.substr(0, VERSION_LENGTH) != version()) {
+    if (channelKeyString.substr(0, VERSION_LENGTH) != Version()) {
       return false;
     }
-
-    std::string rawContent = channelKeyString.substr(VERSION_LENGTH);
-    if (rawContent.empty()) {
-      return false;
-    }
-    DynamicKey6Content content;
-    std::string messageRawContent;
+    int index = 0;
+    index += VERSION_LENGTH;
+    signature_ = channelKeyString.substr(index, SIGNATURE_LENGTH);
+    index += SIGNATURE_LENGTH;
+    app_id_ = channelKeyString.substr(index, APP_ID_LENGTH);
+    index += APP_ID_LENGTH;
     try {
-      Unpack(rawContent, content);
-      messageRawContent = base64Decode(content.message);
-      Unpack(messageRawContent, message_)
-    } catch (std::overflow_error& e) {
+      crc_channel_name_ = std::stoul(
+          channelKeyString.substr(index, UNIX_TS_LENGTH), nullptr, 10);
+      index += UNIX_TS_LENGTH;
+      crc_uid_ = std::stoul(channelKeyString.substr(index, UNIX_TS_LENGTH),
+                            nullptr, 10);
+      index += UNIX_TS_LENGTH;
+      message_raw_content_ =
+          base64Decode(channelKeyString.substr(index, channelKeyString.size()));
+     Unpack(message_raw_content_, message_); 
+
+    } catch (std::exception& e) {
       return false;
     }
-    signature_ = content.signature;
-    app_id_ = content.appID; 
-    message_raw_content_  = messageRawContent;
-    crc_channel_name_ = content.crcChannelName;
-    crc_uid_ = content.crcUid;
     return true;
   }
 
@@ -144,11 +152,11 @@ struct DynamicKey6 {
   std::string uid_;
   std::string signature_;
   std::string app_id_;
-  std::string app_cert;
+  std::string app_cert_;
   PrevilidgeMessage message_;
   std::string message_raw_content_;
   uint32_t crc_channel_name_;
   uint32_t crc_uid_;
 };
-}
-}
+}  // namespace tools
+}  // namespace agora
--- cpp/test/DynamicKey6_test.cpp
+++ cpp/test/DynamicKey6_test.cpp
@@ -4,14 +4,6 @@
 #include <stdint.h>
 using namespace agora::tools;
 
-std::string appID;
-std::string appCertificate;
-std::string channelName;
-uint32_t unixTs;
-uint32_t randomInt;
-uint32_t uid;
-uint32_t expiredTs;
-
 class DynamicKey6_test : public testing::Test
 {
 protected:
@@ -20,15 +12,98 @@ protected:
         appID  = "970CA35de60c44645bbae8a215061b33";
         appCertificate   = "5CFd2fd1755d40ecb72977518be15d3b";
         channelName= "7d72365eb983485397e3e3f9d460bdda";
-        unixTs = 1446455472;
-        randomInt = 58964981;
         uid=2882341273u;
+        uidStr="2882341273";
         expiredTs=1446455471;
     }
+
     virtual void TearDown(){}
 
+    void testDynamicKey(std::string expected, DynamicKey6 key){
+      std::string result = key.Build();
+      EXPECT_EQ(expected, result);
+
+      if (expected == "") {
+        return;
+      }
+      DynamicKey6 k6;
+      bool parsed = k6.FromString(result);
+      ASSERT_TRUE(parsed);
+      EXPECT_EQ(k6.app_id_, key.app_id_);
+      EXPECT_EQ(k6.crc_channel_name_, key.crc_channel_name_);
+      EXPECT_EQ(k6.crc_uid_, key.crc_uid_);
+      uint32_t crc_channel_name = crc32(
+        0, reinterpret_cast<Bytef*>(const_cast<char*>(key.channel_name_.c_str())),
+        key.channel_name_.length());
+      uint32_t crc_uid =
+        crc32(0, reinterpret_cast<Bytef*>(const_cast<char*>(key.uid_.c_str())),
+              key.uid_.length());
+
+      EXPECT_EQ(k6.crc_channel_name_, crc_channel_name);
+      EXPECT_EQ(k6.crc_uid_, crc_uid);
+      EXPECT_EQ(k6.message_.ts, key.message_.ts);
+      EXPECT_EQ(k6.message_.salt, key.message_.salt);
+      EXPECT_EQ(k6.message_.messages[DynamicKey6::Privileges::kJoinChannel], key.message_.messages[DynamicKey6::Privileges::kJoinChannel]);
+
+      std::string signature = DynamicKey6::GenerateSignature(
+          appCertificate
+          , k6.app_id_
+          , key.channel_name_
+          , key.uid_ 
+          , k6.message_raw_content_
+          );
+      EXPECT_EQ(k6.signature_, signature);
+    }
+    void testDynamicKeyWithIntUid()
+    {
+      std::string expected = "006F897E2BFEA30A733B9B4AA05AD6375B456AF483A970CA35de60c44645bbae8a215061b3302766460713847331927AQAAAEf0EAABAAEAryg3Vg==";
+      DynamicKey6 key(appID, appCertificate, channelName, uid);
+      key.message_.salt = 1;
+      key.message_.ts = 1111111;  
+      key.AddPrivilege(DynamicKey6::Privileges::kJoinChannel, expiredTs);
+      testDynamicKey(expected, key);
+    }
+
+    void testDynamicKeyWithStringUid()
+    {
+      std::string expected = "006F897E2BFEA30A733B9B4AA05AD6375B456AF483A970CA35de60c44645bbae8a215061b3302766460713847331927AQAAAEf0EAABAAEAryg3Vg==";
+      DynamicKey6 key(appID, appCertificate, channelName, uidStr);
+      key.AddPrivilege(DynamicKey6::Privileges::kJoinChannel, expiredTs);
+      key.message_.salt = 1;
+      key.message_.ts = 1111111;  
+      testDynamicKey(expected, key);
+    }
+
+    void testDynamicKeyWithErrorUid()
+    {
+      std::string expected = "";
+      DynamicKey6 key(appID, appCertificate, channelName, "error");
+      key.message_.salt = 1;
+      key.message_.ts = 1111111;  
+      key.AddPrivilege(DynamicKey6::Privileges::kJoinChannel, expiredTs);
+      testDynamicKey(expected, key);
+    }
 public:
 
 private:
+    std::string appID;
+    std::string appCertificate;
+    std::string channelName;
+    uint32_t uid;
+    std::string uidStr;
+    uint32_t expiredTs;
 };
+
+TEST_F(DynamicKey6_test, testDynamicKeyWithErrorUid)
+{
+    testDynamicKeyWithErrorUid();
+}
+TEST_F(DynamicKey6_test, testDynamicKeyWithIntUid)
+{
+    testDynamicKeyWithIntUid();
+}
+TEST_F(DynamicKey6_test, testDynamicKeyWithStringUid)
+{
+    testDynamicKeyWithStringUid();
+}
 
