9037cce863735a16037abb93c21f17d8ca26ad9c
ChengleiQiu <457624287@qq.com>
2017-12-20 17:30:33 +0800

    format

--- python/src/SignalingToken.py
+++ python/src/SignalingToken.py
@@ -5,5 +5,5 @@ def getToken(account, appID, appCertificate, expiredTs):
     digest_String = account + appID + appCertificate + str(expiredTs)
     m = hashlib.md5()
     m.update(digest_String)
-    token = m.hexdigest()
-    return "1:" + appID + ":" + str(expiredTs) + ":" + token
+    md5_str = m.hexdigest()
+    return "1:" + appID + ":" + str(expiredTs) + ":" + md5_str
